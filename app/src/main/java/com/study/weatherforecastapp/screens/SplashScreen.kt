961f1d968490d4b6845238187a795cc993a6efd8
Daniel Melo <daniel.melo42@outlook.com>
2023-01-05 20:49:55 -0700

    splash screen logo and animation implemented

--- app/src/main/java/com/study/weatherforecastapp/screens/SplashScreen.kt
+++ app/src/main/java/com/study/weatherforecastapp/screens/SplashScreen.kt
@@ -1,10 +1,78 @@
 package com.study.weatherforecastapp.screens
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.study.weatherforecastapp.R
+import com.study.weatherforecastapp.navigation.AppScreens
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavController) {
-    Text(text = "splash screen")
+    val scale = remember {
+        Animatable(initialValue = 0f)
+    }
+
+    LaunchedEffect(key1 = true, block = {
+        scale.animateTo(
+            targetValue = .9f,
+            animationSpec = tween(
+                durationMillis = 800,
+                easing = {
+                    OvershootInterpolator(2f).getInterpolation(it)
+                }
+            )
+        )
+        delay(timeMillis = 2000L)
+        navController.navigate(route = AppScreens.MainScreen.name)
+    })
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Surface(
+            modifier = Modifier
+                .padding(15.dp)
+                .size(330.dp)
+                .scale(scale = scale.value),
+            shape = CircleShape,
+            border = BorderStroke(width = 2.dp, color = Color.LightGray)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.sun),
+                    contentDescription = "Sun Icon",
+                    modifier = Modifier.size(95.dp),
+                    contentScale = ContentScale.Fit
+                )
+                Text(
+                    text = "Find the Sun?",
+                    style = MaterialTheme.typography.h5,
+                    color = Color.LightGray
+                )
+            }
+        }
+    }
 }
