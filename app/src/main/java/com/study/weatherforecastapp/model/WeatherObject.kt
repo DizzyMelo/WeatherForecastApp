a80cc5932072b13bf632e5c89e111b34bd0368d3
Daniel Melo <daniel.melo42@outlook.com>
2023-01-06 16:59:11 -0700

    models renamed

--- app/src/main/java/com/study/weatherforecastapp/model/WeatherObject.kt
+++ app/src/main/java/com/study/weatherforecastapp/model/WeatherObject.kt
@@ -1,18 +1,8 @@
 package com.study.weatherforecastapp.model
 
 data class WeatherObject(
-    val clouds: Int,
-    val deg: Int,
-    val dt: Int,
-    val feels_like: FeelsLike,
-    val gust: Double,
-    val humidity: Int,
-    val pop: Double,
-    val pressure: Int,
-    val rain: Double,
-    val speed: Double,
-    val sunrise: Int,
-    val sunset: Int,
-    val temp: Temp,
-    val weather: List<WeatherX>
+    val description: String,
+    val icon: String,
+    val id: Int,
+    val main: String
 )
---
+++ app/src/main/java/com/study/weatherforecastapp/repository/WeatherRepository.kt
@@ -0,0 +1,18 @@
+package com.study.weatherforecastapp.repository
+
+import com.study.weatherforecastapp.data.DataOrException
+import com.study.weatherforecastapp.model.Weather
+import com.study.weatherforecastapp.network.WeatherApi
+import javax.inject.Inject
+
+class WeatherRepository @Inject constructor(private val api: WeatherApi) {
+    suspend fun getWeather(city: String, units: String): DataOrException<Weather, Boolean, Exception> {
+        val response = try {
+            api.getWeather(query = city, units = units)
+        } catch (ex: Exception) {
+            return DataOrException(null, false, ex)
+        }
+        return DataOrException(data = response)
+    }
+
+}
