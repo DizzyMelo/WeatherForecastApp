d2ba8236ba6f5c6157fb8d5f290ed2fb5cd4472b
Daniel Melo <daniel.melo42@outlook.com>
2023-01-06 16:58:15 -0700

    api dependency injection function created

--- app/src/main/java/com/study/weatherforecastapp/di/AppModule.kt
+++ app/src/main/java/com/study/weatherforecastapp/di/AppModule.kt
@@ -1,10 +1,26 @@
 package com.study.weatherforecastapp.di
 
+import com.study.weatherforecastapp.network.WeatherApi
+import com.study.weatherforecastapp.util.Constants
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun providesWeatherApi(): WeatherApi {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(WeatherApi::class.java)
+    }
 }
