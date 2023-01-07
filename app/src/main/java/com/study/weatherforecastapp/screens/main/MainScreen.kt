7d795ea7a4253622c14df171ce20d9c2298da793
Daniel Melo <daniel.melo42@outlook.com>
2023-01-07 16:50:29 -0700

    main circular element implemented

--- app/src/main/java/com/study/weatherforecastapp/screens/main/MainScreen.kt
+++ app/src/main/java/com/study/weatherforecastapp/screens/main/MainScreen.kt
@@ -1,25 +1,32 @@
 package com.study.weatherforecastapp.screens.main
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import coil.compose.AsyncImage
 import com.study.weatherforecastapp.components.WeatherAppBar
 import com.study.weatherforecastapp.data.DataOrException
 import com.study.weatherforecastapp.model.Weather
+import com.study.weatherforecastapp.util.Constants
 
 @Composable
 fun MainScreen(navController: NavHostController, viewModel: MainViewModel = hiltViewModel()) {
     val weatherData = produceState<DataOrException<Weather, Boolean, Exception>>(
         initialValue = DataOrException(loading = true), producer = {
-            value = viewModel.getWeatherData("Seattle")
+            value = viewModel.getWeatherData("natal")
         }
     ).value
 
@@ -47,7 +54,55 @@ fun MainScaffold(weather: Weather, navController: NavController) {
 
 @Composable
 fun MainContent(weather: Weather) {
-    Text(text = "Main Content")
+    val imageUrl = "${Constants.IMG_BASE_URL}${weather.list.first().weather.first().icon}.png"
+    Column(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+        Text(
+            text = "Nov 29, Mon",
+            style = MaterialTheme.typography.caption,
+            color = MaterialTheme.colors.onSecondary,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(6.dp)
+        )
+        Surface(
+            modifier = Modifier
+                .padding(4.dp)
+                .size(200.dp),
+            shape = CircleShape,
+            color = Color(0xFFFFC400)
+        ) {
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                    WeatherStateImage(imageUrl)
+                    Text(
+                        text = "54",
+                        style = MaterialTheme.typography.h4,
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                    Text(
+                        text = "Rain",
+                        fontStyle = FontStyle.Italic
+                    )
+                }
+            
+        }
+    }
+}
+
+@Composable
+fun WeatherStateImage(imageUrl: String) {
+    AsyncImage(
+        model = imageUrl,
+        contentDescription = "Weather representation icon",
+        modifier = Modifier.size(80.dp)
+    )
 }
 
 @Preview
