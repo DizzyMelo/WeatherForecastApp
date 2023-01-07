ef403d3d96d408c9fce2779ac0cfb808fd5d8ff2
Daniel Melo <daniel.melo42@outlook.com>
2023-01-07 11:30:45 -0700

    main screen restructured

--- app/src/main/java/com/study/weatherforecastapp/screens/main/MainScreen.kt
+++ app/src/main/java/com/study/weatherforecastapp/screens/main/MainScreen.kt
@@ -1,21 +1,19 @@
 package com.study.weatherforecastapp.screens.main
 
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Text
+import android.annotation.SuppressLint
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import com.study.weatherforecastapp.components.WeatherAppBar
 import com.study.weatherforecastapp.data.DataOrException
 import com.study.weatherforecastapp.model.Weather
 
 @Composable
 fun MainScreen(navController: NavHostController, viewModel: MainViewModel = hiltViewModel()) {
-    ShowData(viewModel)
-}
-
-@Composable
-fun ShowData(viewModel: MainViewModel) {
     val weatherData = produceState<DataOrException<Weather, Boolean, Exception>>(
         initialValue = DataOrException(loading = true), producer = {
             value = viewModel.getWeatherData("Seattle")
@@ -24,8 +22,27 @@ fun ShowData(viewModel: MainViewModel) {
 
     if (weatherData.loading == true) {
         CircularProgressIndicator()
-    } else if (weatherData.data != null){
-        Text(text = "Main Screen ${weatherData.data!!.city.country}")
+    } else if (weatherData.data != null) {
+        MainScaffold(weather = weatherData.data!!, navController = navController)
     }
+}
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun MainScaffold(weather: Weather, navController: NavController) {
+    Scaffold(topBar = {
+        WeatherAppBar(title = "${weather.city.name}, ${weather.city.country}")
+    }) {
+        MainContent(weather = weather)
+    }
+}
+
+@Composable
+fun MainContent(weather: Weather) {
+    Text(text = "Main Content")
+}
+
+@Preview
+@Composable
+fun MainScaffoldPreview() {
 }
