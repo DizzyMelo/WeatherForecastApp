f8d1ccadf5ee7b73587adb7802ac9be9c2586944
Daniel Melo <daniel.melo42@outlook.com>
2023-01-10 11:23:58 -0700

    favorite dao interface created

---
+++ app/src/main/java/com/study/weatherforecastapp/data/WeatherDao.kt
@@ -0,0 +1,32 @@
+package com.study.weatherforecastapp.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.study.weatherforecastapp.model.Favorite
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface WeatherDao {
+
+    @Query("SELECT * FROM favorite_tbl")
+    fun getFavorites(): Flow<List<Favorite>>
+
+    @Query("SELECT * FROM favorite_tbl WHERE city = :city")
+    suspend fun getFavoriteById(city: String): Favorite
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavorite(favorite: Favorite)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateFavorite(favorite: Favorite)
+
+    @Query("DELETE FROM favorite_tbl")
+    suspend fun deleteAllFavorites()
+
+    @Delete
+    suspend fun deleteFavorite(favorite: Favorite)
+}
