c80682b4da310f3b89e6a0cffda8f182ca2e92c1
Daniel Melo <daniel.melo42@outlook.com>
2023-01-10 11:35:36 -0700

    dependency injection functions implemented

--- app/src/main/java/com/study/weatherforecastapp/di/AppModule.kt
+++ app/src/main/java/com/study/weatherforecastapp/di/AppModule.kt
@@ -1,10 +1,15 @@
 package com.study.weatherforecastapp.di
 
+import android.content.Context
+import androidx.room.Room
+import com.study.weatherforecastapp.data.WeatherDao
+import com.study.weatherforecastapp.data.WeatherDatabase
 import com.study.weatherforecastapp.network.WeatherApi
 import com.study.weatherforecastapp.util.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -14,6 +19,19 @@ import javax.inject.Singleton
 @Module
 object AppModule {
 
+    @Singleton
+    @Provides
+    fun provideWeatherDao(weatherDatabase: WeatherDatabase): WeatherDao = weatherDatabase.weatherDao()
+
+    @Singleton
+    @Provides
+    fun provideWeatherDatabase(@ApplicationContext context: Context): WeatherDatabase
+    = Room.databaseBuilder(
+        context,
+        WeatherDatabase::class.java,
+        "weather_database")
+        .fallbackToDestructiveMigration()
+        .build()
     @Singleton
     @Provides
     fun providesWeatherApi(): WeatherApi {
