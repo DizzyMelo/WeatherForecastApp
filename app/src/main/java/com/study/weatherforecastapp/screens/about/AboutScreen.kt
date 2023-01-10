55618293ebc0ba8e09c38b72a436155b20d0b91a
Daniel Melo <daniel.melo42@outlook.com>
2023-01-10 11:05:48 -0700

    about app string added and information displayed on the about screen

--- app/src/main/java/com/study/weatherforecastapp/screens/about/AboutScreen.kt
+++ app/src/main/java/com/study/weatherforecastapp/screens/about/AboutScreen.kt
@@ -1,10 +1,54 @@
 package com.study.weatherforecastapp.screens.about
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.study.weatherforecastapp.R
+import com.study.weatherforecastapp.components.WeatherAppBar
+import com.study.weatherforecastapp.util.Constants
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun AboutScreen(navController: NavController) {
-    Text(text = "About")
+    Scaffold(topBar = {
+        WeatherAppBar(
+            title = "About",
+            navController = navController,
+            isMainScreen = false,
+            elevation = 0.dp,
+            icon = Icons.Default.ArrowBack,
+            onButtonClicked = {navController.popBackStack()}
+        )
+    }) {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(id = R.string.about_app),
+                    style = MaterialTheme.typography.subtitle1,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Weather Api by: ${Constants.API_BASE_URL}",
+                    style = MaterialTheme.typography.body1
+                )
+            }
+        }
+    }
 }
