a61b46d6a893b52245090d813ff8ed8f10279302
Daniel Melo <daniel.melo42@outlook.com>
2023-01-11 21:40:00 -0700

    added units (imperial and metric) constants

--- app/src/main/java/com/study/weatherforecastapp/util/Constants.kt
+++ app/src/main/java/com/study/weatherforecastapp/util/Constants.kt
@@ -4,4 +4,7 @@ object Constants {
     const val API_BASE_URL = "https://api.openweathermap.org/"
     const val IMG_BASE_URL = "https://openweathermap.org/img/wn/"
     const val API_KEY = "ed60fcfbd110ee65c7150605ea8aceea"
+
+    const val IMPERIAL_UNIT = "imperial"
+    const val METRIC_UNIT = "metric"
 }
