1260a83bfaeb16afef039a03cffade3d7daa5ae9
Daniel Melo <daniel.melo42@outlook.com>
2023-01-11 21:06:56 -0700

    settings table with unit created

--- app/src/main/java/com/study/weatherforecastapp/data/WeatherDatabase.kt
+++ app/src/main/java/com/study/weatherforecastapp/data/WeatherDatabase.kt
@@ -3,8 +3,9 @@ package com.study.weatherforecastapp.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.study.weatherforecastapp.model.Favorite
+import com.study.weatherforecastapp.model.Unit
 
-@Database(entities = [Favorite::class], version = 1, exportSchema = false)
+@Database(entities = [Favorite::class, Unit::class], version = 2, exportSchema = false)
 abstract class WeatherDatabase: RoomDatabase() {
     abstract fun weatherDao(): WeatherDao
 }
---
+++ app/src/main/java/com/study/weatherforecastapp/model/Unit.kt
@@ -0,0 +1,12 @@
+package com.study.weatherforecastapp.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "settings_tbl")
+data class Unit(
+    @PrimaryKey
+    @ColumnInfo(name = "unit")
+    val unit: String
+)
