cd972029aa31000f8ee75cbba81456205fdc0d7f
Daniel Melo <daniel.melo42@outlook.com>
2023-01-11 21:07:18 -0700

    dao functions added

--- app/src/main/java/com/study/weatherforecastapp/data/WeatherDao.kt
+++ app/src/main/java/com/study/weatherforecastapp/data/WeatherDao.kt
@@ -7,10 +7,13 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import com.study.weatherforecastapp.model.Favorite
+import com.study.weatherforecastapp.model.Unit
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface WeatherDao {
+
+    // CRUD for favorite table
 
     @Query("SELECT * FROM favorite_tbl")
     fun getFavorites(): Flow<List<Favorite>>
@@ -29,4 +32,21 @@ interface WeatherDao {
 
     @Delete
     suspend fun deleteFavorite(favorite: Favorite)
+
+    // CRUD for settings table
+    @Query("SELECT * FROM settings_tbl")
+    fun getUnits(): Flow<List<Unit>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUnit(unit: Unit)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateUnit(unit: Unit)
+
+    @Query("DELETE FROM settings_tbl")
+    suspend fun deleteAllUnits()
+
+    @Delete
+    suspend fun deleteUnit(unit: Unit)
+
 }
--- app/src/main/java/com/study/weatherforecastapp/repository/WeatherDbRepository.kt
+++ app/src/main/java/com/study/weatherforecastapp/repository/WeatherDbRepository.kt
@@ -2,6 +2,7 @@ package com.study.weatherforecastapp.repository
 
 import com.study.weatherforecastapp.data.WeatherDao
 import com.study.weatherforecastapp.model.Favorite
+import com.study.weatherforecastapp.model.Unit
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
@@ -12,4 +13,12 @@ class WeatherDbRepository @Inject constructor(private val dao: WeatherDao) {
     suspend fun updateFavorite(favorite: Favorite) = dao.updateFavorite(favorite)
     suspend fun deleteAllFavorites() = dao.deleteAllFavorites()
     suspend fun deleteFavorite(favorite: Favorite) = dao.deleteFavorite(favorite)
+
+    // CRUD for settings table
+
+    fun getUnits(): Flow<List<Unit>> = dao.getUnits()
+    suspend fun addUnit(unit: Unit) = dao.insertUnit(unit)
+    suspend fun updateUnit(unit: Unit) = dao.updateUnit(unit)
+    suspend fun deleteUnit(unit: Unit) = dao.deleteUnit(unit)
+    suspend fun deleteAllUnits() = dao.deleteAllUnits()
 }
